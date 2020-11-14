dd1dd4fbc26ae28a8766b81319a0a059a1b7ac57
bestOnly <956077132@qq.com>
2020-11-14 20:56:22 +0800

    'mode'

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  text-align: center;
+  font-size: 50px;
+  font-weight: bold;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+     欢迎来到VUE世界
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {}
+  },
+  methods () {},
+  create () {},
+  computed () {}
+}
+</script>
